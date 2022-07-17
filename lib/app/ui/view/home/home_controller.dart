f9ac30166ffd74855fc88fbe556ef675c66c2cf5
Abdullah T <abdullahozvanli@gmail.com>
2022-07-17 21:28:00 +0300

    #002_home
    YENI FONTLAR EKLENDİ
    TEXTSTYLE DÜZENLENDİ
    SLİDER INDICATOR DUZENLENDİ
    ANKET KARTI OLUŞTURULDU
    OY VEREN KULLANICI WİDGETİ OLUŞTURULDU

--- lib/app/ui/view/home/home_controller.dart
+++ lib/app/ui/view/home/home_controller.dart
@@ -1,6 +1,21 @@
 import 'package:apmatik_app/app/core/base/base_controller.dart';
+import 'package:carousel_slider/carousel_controller.dart';
 
 class HomeController extends BaseController {
+
+
+  final List<String> imgList = [
+    'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+    'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
+    'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
+    'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+    'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
+    'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
+  ];
+  int current = 0;
+  final CarouselController carouselController = CarouselController();
+
+
   @override
   void onInit() {
     box.write('isLogin', true);
---
+++ lib/app/ui/widgets/questionnaire_and_sponsored_card.dart
@@ -0,0 +1,179 @@
+import 'package:apmatik_app/app/core/constant/color_constants.dart';
+import 'package:apmatik_app/app/ui/style/text_style.dart';
+import 'package:apmatik_app/app/ui/widgets/stacked_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class QuestionnaireAndSponsoredCardWidget extends StatelessWidget {
+  const QuestionnaireAndSponsoredCardWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 20),
+      width: 350,
+      height: 307,
+      child: Column(
+        children: <Widget>[
+          Card(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Stack(
+                  children: [
+                    Image.asset(
+                      'assets/images/anket.png',
+                      width: 350,
+                      height: 135,
+                      fit: BoxFit.fill,
+                    ),
+                    Align(
+                      alignment: Alignment.topRight,
+                      child: Container(
+                        width: 70.w,
+                        height: 20.h,
+                        decoration: BoxDecoration(
+                            color: AppColors.ORANGE,
+                            borderRadius: BorderRadius.only(
+                                bottomLeft: Radius.circular(30))),
+                        child: Text(
+                          'Anket',
+                          textAlign: TextAlign.center,
+                          style: AppTextStyle()
+                              .getSfProDisplayRegular_H5(AppColors.WHITE),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 10.h,
+                ),
+                ListTile(
+                  title: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        'Apartman Boyanması',
+                        style: AppTextStyle().getSfProRoundedSemiBold_h5(
+                          AppColors.BLACK,
+                        ),
+                      ),
+                      Text(
+                        '8 saat önce',
+                        style: AppTextStyle()
+                            .getSfProDisplayRegular_Italic_H6(
+                                AppColors.GREY_OPACITY),
+                      ),
+                    ],
+                  ),
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Apartman Boyanması için apartmanımızda anket başlamıştır. Hemen katılıp bizimle fikirlerini paylaş!',
+                        style: AppTextStyle()
+                            .getSfProDisplayRegular_H6(AppColors.BLACK),
+                      ),
+                      buildStackedImages(direction: TextDirection.rtl),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20.0.w),
+                  child: TextButton(
+                      child: Padding(
+                        padding:
+                            EdgeInsets.only(left: 8.0.w, right: 8.0.w),
+                        child: Text("Detaylar",
+                            style: AppTextStyle()
+                                .getSfProDisplayRegular_H5(
+                                    AppColors.ORANGE)),
+                      ),
+                      style: ButtonStyle(
+                        shape: MaterialStateProperty.all<
+                            RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5),
+                            side: BorderSide(color: AppColors.ORANGE),
+                          ),
+                        ),
+                        fixedSize: MaterialStateProperty.all<Size>(
+                          Size(89.w, 27.h),
+                        ),
+                      ),
+                      onPressed: () => null),
+                ),
+                SizedBox(
+                  height: 17.h,
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+Widget buildStackedImages({
+  TextDirection direction = TextDirection.ltr,
+}) {
+  final double size = 30;
+  final double xShift = 10;
+  final urlImages = [
+    'https://images.unsplash.com/photo-1554151228-14d9def656e4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=633&q=80',
+    'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
+    'https://images.unsplash.com/photo-1616766098956-c81f12114571?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
+  ];
+
+  final items = urlImages.map((urlImage) => buildImage(urlImage)).toList();
+
+  return StackedWidgets(
+    direction: direction,
+    items: [
+      ...items,
+      ClipOval(
+        child: Container(
+          padding: EdgeInsets.all(5),
+          color: Colors.white,
+          child: ClipOval(
+              child: Container(
+                  color: AppColors.ORANGE,
+                  child: Center(
+                    child: Text(
+                      '+22',
+                      style: AppTextStyle()
+                          .getSfProRoundedSemiBold_h6(AppColors.WHITE),
+                    ),
+                  ))),
+        ),
+      )
+    ],
+    size: size,
+    xShift: xShift,
+    label: "Bu anketi oyladı",
+  );
+}
+
+Widget buildImage(String urlImage) {
+  final double borderSize = 5;
+
+  return ClipOval(
+    child: Container(
+      padding: EdgeInsets.all(borderSize),
+      color: Colors.white,
+      child: ClipOval(
+        child: Image.network(
+          urlImage,
+          fit: BoxFit.cover,
+        ),
+      ),
+    ),
+  );
+}
